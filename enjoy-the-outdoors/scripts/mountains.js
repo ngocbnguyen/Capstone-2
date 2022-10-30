1544d93ce15d2a6b27c7f9672a2e9c734651d3ec
Ngoc <ngocbnguyen19@gmail.com>
2022-10-30 16:50:02 -0400

    mountain page

---
+++ enjoy-the-outdoors/scripts/mountains.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const mountainField = document.getElementById("mountainsField");
+const cardSection = document.getElementById("card-section");
+
+function loadMountSelect() {
+  mountainField.innerHTML = "";
+  let option = new Option("Select...", " ");
+  mountainField.appendChild(option);
+  mountainsArray.forEach((mountain) => {
+    let newOption = new Option(mountain.name, mountain.name);
+    mountainField.appendChild(newOption);
+  });
+}
+
+function loadMountainInfo() {
+  let selectedValue = mountainField.value;
+  mountainsArray.forEach((mountain) => {
+    if (selectedValue === mountain.name) {
+      buildMountainCard(cardSection, mountain);
+    }
+  });
+}
+
+
+function buildMountainCard(section, mountain) {
+  let colDiv = document.createElement("div");
+  colDiv.className = "col";
+
+  const div = document.createElement("div");
+  div.className = "card p-3";
+  div.style = "width: 22em;";
+  section.appendChild(colDiv);
+  colDiv.appendChild(div);
+
+  let cardImg = document.createElement("img");
+  cardImg.className = "card-img-top";
+  cardImg.alt = mountain.name;
+  cardImg.src = "../images/" + mountain.img;
+
+  let cardTitle = document.createElement("h5");
+  cardTitle.className = "card-title";
+  cardTitle.innerText = mountain.name;
+
+  let desc = document.createElement("p");
+  desc.innerText = mountain.desc;
+  let elevation = document.createElement("p");
+  elevation.innerText = `${mountain.elevation} ft`;
+  let addInfo = document.createElement("p");
+  addInfo.innerText = `Effort: ${mountain.effort}
+    Coordinates: ${mountain.coords.lat}, ${mountain.coords.lng}`;
+
+  const divBody = document.createElement("div");
+  divBody.className = "card-body";
+  div.append(cardImg, cardTitle, desc, elevation, addInfo);
+}
+
+  window.onload = () => {
+    loadMountSelect();
+    mountainField.onchange = loadMountainInfo;
+  }
